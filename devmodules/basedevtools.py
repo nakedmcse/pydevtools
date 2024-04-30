e74208cbe31297aa0d58077ded92577a0e41df18
Walker Aldridge <daldridge@lairdscomputer.com>
2024-04-30 08:57:54 -0500

    Add default render implementation to base module class

--- devmodules/basedevtools.py
+++ devmodules/basedevtools.py
@@ -1,5 +1,16 @@
 # Base module class
+import tkinter as tk
+
+
 class basedevtools:
     def __init__(self, display_name: str, category: str):
         self.display_name = display_name
         self.category = category
+
+    def render(self, output_frame):
+        output_text = tk.Text(output_frame, state=tk.DISABLED)
+        output_text.pack(expand=True, fill=tk.BOTH)
+        output_text.config(state=tk.NORMAL)
+        output_text.delete(1.0, tk.END)
+        output_text.insert(tk.END, self.display_name)
+        output_text.config(state=tk.DISABLED)
--- devmodules/encoding_tool_1.py
+++ devmodules/encoding_tool_1.py
@@ -1,18 +1,7 @@
 # Test module
-import tkinter as tk
-from tkinter import ttk, font
-
 from devmodules import basedevtools as base
 
 
 class test_module_3(base.basedevtools):
     display_name = "Some Encoding Tool"
     category = "Encoding tools"
-
-    def render(self, output_frame):
-        output_text = tk.Text(output_frame, state=tk.DISABLED)
-        output_text.pack(expand=True, fill=tk.BOTH)
-        output_text.config(state=tk.NORMAL)
-        output_text.delete(1.0, tk.END)
-        output_text.insert(tk.END, f"Some Encoding Tool\n")
-        output_text.config(state=tk.DISABLED)
