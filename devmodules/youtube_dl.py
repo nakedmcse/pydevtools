fed94bd1869cfbbc685dae90b2f27238e4387665
Walker Aldridge <daldridge@lairdscomputer.com>
2024-05-23 09:19:33 -0500

    Update selector layout

--- devmodules/youtube_dl.py
+++ devmodules/youtube_dl.py
@@ -15,6 +15,11 @@ class youtube_dl(base.basedevtools):
     resolution_var = None
     root = None
 
+    def get_highest_itag(self, videos, mime):
+        filtered = [video for video in videos if video['mimeType'].startswith(mime)]
+        hq = max(filtered, key=lambda x: int(x['qualityLabel'].rstrip('p')), default=None)
+        return hq['itag'] if hq else None
+
     def download(self):
         url = self.url_entry.get()
         if url:
@@ -26,11 +31,13 @@ class youtube_dl(base.basedevtools):
                 resolution = self.resolution_var.get()
                 match resolution:
                     case "Highest":
-                        yt_object = yt_object.streams.get_highest_resolution()
+                        yt_object = yt_object.streams.filter(progressive=True).get_highest_resolution()
                     case "Lowest":
-                        yt_object = yt_object.streams.get_lowest_resolution()
+                        yt_object = yt_object.streams.filter(progressive=True).get_lowest_resolution()
                     case "Audio":
-                        yt_object = yt_object.streams.get_audio_only()
+                        yt_object = yt_object.streams.filter(progressive=True).get_audio_only()
+                    case _:
+                        yt_object = yt_object.streams.filter(progressive=True).get_highest_resolution()
 
                 filepath = filedialog.askdirectory()
                 savedpath = yt_object.download(output_path=filepath)
@@ -60,14 +67,14 @@ class youtube_dl(base.basedevtools):
         button_frame = tk.Frame(output_frame)
         button_frame.pack(fill=tk.X, pady=5)
 
-        self.url_entry = tk.Entry(button_frame, width=40)
+        self.url_entry = tk.Entry(button_frame, width=50)
         self.url_entry.pack(side="left", padx=5)
 
         resolution_label = tk.Label(button_frame, text="Resolution:")
         resolution_label.pack(side="left")
         self.resolution_var = tk.StringVar(value="Highest")
         resolution_menu = tk.OptionMenu(button_frame, self.resolution_var, "Highest", "Lowest", "Audio")
-        resolution_menu.config(width=8)
+        resolution_menu.config(width=6)
         resolution_menu.pack(side="left", padx=5)
 
         download_button = tk.Button(button_frame, text="Download", command=self.download)
