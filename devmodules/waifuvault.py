00f297391a3e2ce009139319a456288f53d900a0
Walker Aldridge <daldridge@lairdscomputer.com>
2024-07-01 09:07:29 -0500

    Waifuvault WIP

--- devmodules/waifuvault.py
+++ devmodules/waifuvault.py
@@ -14,6 +14,11 @@ class waifuvault_ul(base.basedevtools):
     root = None
     hidefilename_var = None
     onetime_var = None
+    password_var = None
+    expire_var = None
+
+    def upload_file(self):
+        file_path = filedialog.askopenfilename()
 
     def render(self, output_frame):
         self.root = output_frame
@@ -23,6 +28,8 @@ class waifuvault_ul(base.basedevtools):
 
         button_frame = tk.Frame(output_frame)
         button_frame.pack(fill=tk.X, pady=5)
+        button_lower_frame = tk.Frame(output_frame)
+        button_lower_frame.pack(fill=tk.X)
 
         self.hidefilename_var = tk.BooleanVar(value=False)
         hidefilename_flag = tk.Checkbutton(button_frame, text="Hide Filename", variable=self.hidefilename_var)
@@ -32,14 +39,17 @@ class waifuvault_ul(base.basedevtools):
         onetime_flag = tk.Checkbutton(button_frame, text="One Time Download", variable=self.onetime_var)
         onetime_flag.pack(side="left", padx=5)
 
-        upload_button = tk.Button(button_frame, text="Upload", command=self.upload_file)
+        expire_label = tk.Label(button_frame, text="Expire:")
+        expire_label.pack(side="left")
+        self.expire_var = tk.StringVar(value="Compute")
+        expire_menu = tk.OptionMenu(button_frame, self.expire_var, "Compute", "1 Hour", "1 Day", "1 Week", "1 Month", "1 Year")
+        expire_menu.config(width=6)
+        expire_menu.pack(side="left", padx=5)
+
+        password_label = tk.Label(button_frame, text="Password:")
+        password_label.pack(side="left", padx=5)
+        self.password_var = tk.Entry(button_frame, width=20)
+        self.password_var.pack(side="left", padx=5)
+
+        upload_button = tk.Button(button_lower_frame, text="Upload", command=self.upload_file)
         upload_button.pack(side="left", padx=5)
-
-        delete_button = tk.Button(button_frame, text="Delete", command=self.delete_file)
-        delete_button.pack(side="left", padx=5)
-
-        self.comp_level = tk.Label(button_frame)
-        self.comp_level.pack(side="left", padx=10)
-
-        self.image_label = tk.Label(output_frame, width=80)
-        self.image_label.pack(side="left", fill=tk.BOTH, expand=True, padx=5)
